29c6872bb154fcac31179f6593bd49a09bbab5a1
Adicshan <129241416+Adicshan@users.noreply.github.com>
2023-09-01 14:58:19 +0530

    Update Exam_prediction.py

--- Exam_prediction.py
+++ Exam_prediction.py
@@ -6,7 +6,6 @@ sys.path.append(pandas_path)
 sys.path.append(sklearn_path)
 python_version=sys.version
 print(python_version)
-# Print sys.path to verify that the paths are added correctly
 print(sys.path)
 import math
 import pandas as pd
